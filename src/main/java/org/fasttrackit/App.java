2337e4d362d67ec4b950dd58678c5f316c4ff1f2
andreicacipri <cipri9412@gmail.com>
2018-12-12 21:14:19 +0200

    Lab 3, Classes, variables and primitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // declare a variable and instanting a Car object
+       Car firstCompetitor = new Car();
+       firstCompetitor.name = "Dacia";
+       firstCompetitor.color = "black";
+       firstCompetitor.doorCount = 6;
+       firstCompetitor.mileage = 9.5;
+       Engine firstEngine =new Engine();
+       firstEngine.manufacturer = "Renault";
+        //local variables are variables declared inside main
+        //they don;t receive default values
+        Car secondCompetitor =new Car();
+        secondCompetitor.name = "Golf";
+        // sout print a message to the console
+       System.out.println(firstEngine.manufacturer);
+       firstCompetitor.engine = firstEngine;
+        firstCompetitor.engine.manufacturer ="BMW";
+        System.out.println(firstEngine.manufacturer);
     }
 }
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,17 @@
+package org.fasttrackit;
+
+public class Car {
+// instance variables (declared as a propperties of a class)
+    // they receive default values( null for objects, 0 for numbers,false for boolean)
+    String name;
+    String color;
+    int doorCount;
+    double mileage;
+
+    boolean running;
+    long totalKm;
+    Engine engine;
+
+
+
+}
