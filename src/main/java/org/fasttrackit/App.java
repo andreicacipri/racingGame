05c7f7cdf23605bc242552ed970720d570736a9b
andreicacipri <cipri9412@gmail.com>
2019-01-16 20:20:43 +0200

    studied array and for loops

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,7 +1,5 @@
 package org.fasttrackit;
 
-import org.fasttrackit.cheater.CheatingCar;
-
 /**
  * Hello world!
  *
@@ -10,41 +8,51 @@ public class App
 {
     public static void main( String[] args )
     {
-//        // declare a variable and instanting a Car object
-       Car firstCompetitor = new Car(new Engine());
-       firstCompetitor.setName("Dacia");
-       firstCompetitor.setColor("black");
-       firstCompetitor.doorCount = 6;
-       firstCompetitor.setMileage(9.5);
-       Engine firstEngine =new Engine();
-       firstEngine.manufacturer = "Renault";
-        //local variables are variables declared inside main
-        //they don;t receive default values
-        Car secondCompetitor =new Car(new Engine());
-        secondCompetitor.setName("Golf");
-        // sout print a message to the console
-       System.out.println(firstEngine.manufacturer);
-       firstCompetitor.engine = firstEngine;
-        firstCompetitor.engine.manufacturer ="BMW";
-        System.out.println(firstEngine.manufacturer);
+       Game game = new Game();
+       game.addTrack(0,new Track("Highway",300 ));
+       game.addTrack(1,new Track("Seaside",100 ));
+       game.addTrack(2,new Track("Dakar",250 ));
+        game.displayAvailableTracks();
 
-       double traveledDistance = firstCompetitor.accelerate(100, 30);
-        System.out.println(traveledDistance);
-     Vehicle vehicle = new Vehicle();
-     Vehicle.applicationTotalNumberOfVehicles = 1;
-        System.out.println("Total from vehicle 1: "+ Vehicle.applicationTotalNumberOfVehicles);
-        Vehicle vehicle2 = new Vehicle();
-        Vehicle.applicationTotalNumberOfVehicles = 2;
-        System.out.println("Total from vehicle 2: "+ Vehicle.applicationTotalNumberOfVehicles);
-        System.out.println("Total from vehicle 1: "+ Vehicle.applicationTotalNumberOfVehicles);
-        CheatingCar firstCheatingCar = new CheatingCar();
-        firstCheatingCar.name = "Logan";
-        firstCheatingCar.accelerate(2,2);
 
-        Vehicle cheatingCar = new CheatingCar();
-        cheatingCar.accelerate(10,2);
-        //type casting
-        ((CheatingCar) cheatingCar).test();
+
+//       // declare a variable and instanting a Car object
+//    Car firstCompetitor = new Car(new Engine());
+//    firstCompetitor.setName("Dacia");
+//    firstCompetitor.setColor("black");
+//    firstCompetitor.doorCount = 6;
+//    firstCompetitor.setMileage(9.5);
+//    Engine firstEngine =new Engine();
+//    firstEngine.manufacturer = "Renault";
+//     //local variables are variables declared inside main
+//     //they don;t receive default values
+//     Car secondCompetitor =new Car(new Engine());
+//     secondCompetitor.setName("Golf");
+//     // sout print a message to the console
+//    System.out.println(firstEngine.manufacturer);
+//    firstCompetitor.engine = firstEngine;
+//     firstCompetitor.engine.manufacturer ="BMW";
+//     System.out.println(firstEngine.manufacturer);
+
+//    double traveledDistance = firstCompetitor.accelerate(100, 30);
+//     System.out.println(traveledDistance);
+//  Vehicle vehicle = new Vehicle();
+//  Vehicle.applicationTotalNumberOfVehicles = 1;
+//     System.out.println("Total from vehicle 1: "+ Vehicle.applicationTotalNumberOfVehicles);
+//     Vehicle vehicle2 = new Vehicle();
+//     Vehicle.applicationTotalNumberOfVehicles = 2;
+//     System.out.println("Total from vehicle 2: "+ Vehicle.applicationTotalNumberOfVehicles);
+//     System.out.println("Total from vehicle 1: "+ Vehicle.applicationTotalNumberOfVehicles);
+//     CheatingCar firstCheatingCar = new CheatingCar();
+//     firstCheatingCar.name = "Logan";
+//     firstCheatingCar.accelerate(2,2);
+
+//     Vehicle cheatingCar = new CheatingCar();
+//     cheatingCar.accelerate(10,2);
+//     //type casting
+//     ((CheatingCar) cheatingCar).test();
+
+
 
     }
 
---
+++ src/main/java/org/fasttrackit/Game.java
@@ -0,0 +1,26 @@
+package org.fasttrackit;
+
+public class Game {
+    private Track[] tracks = new Track[10];
+
+    public void addTrack(int index, Track track) {
+        tracks[index] = track;
+    }
+
+    public void displayAvailableTracks() {
+        System.out.println("Available tracks");
+//        // for loop
+//        for (int i = 0; i < tracks.length; i++) {
+//            if (tracks[i] != null) {
+//                System.out.println(tracks[i].getName());
+//            }
+//        }
+//    }
+        //enhanced for loop (for-each)
+        for (Track track : tracks) {
+            if (track != null) {
+                System.out.println(track.getName());
+            }
+        }
+    }
+}
